969e7ca1230e5de99d9a90538df03b79df18914e
ducnhat99 <nguyenvanducnhat@gmail.com>
2021-08-19 19:51:14 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Search from "./components/common/card/Search";
+import Navigation from "./components/common/card/Navigation";
+function App() {
+  return (
+    <>
+      <Search />
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
