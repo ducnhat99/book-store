043b1cd62a1045786e30ba2b1087c5d5135195e0
ducnhat99 <nguyenvanducnhat@gmail.com>
2021-08-20 11:24:28 +0700

    add side bar and fix slide show

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Search from "./components/common/card/Search";
 import Navigation from "./components/common/card/Navigation";
 import SlideShow from "./components/common/card/SlideShow";
+import SideBar from "./components/common/card/SideBar";
+
 function App() {
   return (
     <>
-      {/* <Search />
-      <Navigation /> */}
-      <SlideShow />
+      <SideBar />
     </>
   );
 }
