1f2bffab948f1d763f83fac664fe736f27260ffc
ducnhat99 <nguyenvanducnhat@gmail.com>
2021-08-21 10:02:03 +0700

    add new card product item

--- src/components/common/card/NewCardItem.js
+++ src/components/common/card/NewCardItem.js
@@ -1,9 +1,19 @@
 import '../../../styles/new-card-item.css'
-
+import images from '../../../images/7kyquanthegioi.jpg'
 const NewCardItem = () => {
     return (
         <div className="new-card">
-
+            <div className="new-card-content">
+                <div className="new-card-images">
+                    <img src={images}></img>
+                </div>
+                <div className="new-card-title">
+                    <h3>Sài Gòn - Những Mảnh Ghép Rời Ký Ức Sài Gòn Sài Gòn</h3>
+                </div>
+                <div className="new-card-item-price">
+                    <p>50000d</p>
+                </div>
+            </div>
         </div>
     )
 }
