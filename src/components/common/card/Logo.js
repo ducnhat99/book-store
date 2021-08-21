ff465fa8234acb0a31b5ae376c41a75355901064
ducnhat99 <nguyenvanducnhat@gmail.com>
2021-08-21 16:13:49 +0700

    responsive header iphoneX, ipap pro

---
+++ src/components/common/card/Logo.js
@@ -0,0 +1,11 @@
+import '../../../styles/logo.css'
+import logo from '../../../images/logo.png'
+const Logo = () => {
+    return (
+        <div className="logo">
+            <img src={logo} alt="logo"></img>
+        </div>
+    )
+}
+
+export default Logo
