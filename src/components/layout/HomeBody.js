cddf62a77560ee95578c94b0bfd70e151c268397
MacXuanHung <humanscoffee@gmail.com>
2021-09-01 10:11:46 +0700

    commit notification

--- src/components/layout/HomeBody.js
+++ src/components/layout/HomeBody.js
@@ -1,24 +1,24 @@
-import SideBar from '../common/card/SideBar'
-import SlideShow from '../common/card/SlideShow'
-import NewCardContainer from '../common/card/NewCardContainer'
-import SaleCardContainer from '../common/card/SaleCardContainer'
-import '../../styles/homebody.css'
-import FeaturedCardContainer from '../common/card/FeaturedCardContainer'
+import SideBar from '../common/card/SideBar';
+import SlideShow from '../common/card/SlideShow';
+import NewCardContainer from '../common/card/NewCardContainer';
+import SaleCardContainer from '../common/card/SaleCardContainer';
+import '../../styles/homebody.css';
+import FeaturedCardContainer from '../common/card/FeaturedCardContainer';
 
 const HomeBody = () => {
-    return (
-        <div className="home--body">
-            <div className="container">
-                <div className="homebody-menu">
-                    <SideBar />
-                    <SlideShow />
-                </div>
-                <NewCardContainer />
-                <SaleCardContainer />
-                <FeaturedCardContainer />
-            </div>
+  return (
+    <div className="home--body">
+      <div className="container">
+        <div className="homebody-menu">
+          <SideBar />
+          <SlideShow />
         </div>
-    )
-}
+        <NewCardContainer />
+        <SaleCardContainer />
+        <FeaturedCardContainer />
+      </div>
+    </div>
+  );
+};
 
-export default HomeBody
+export default HomeBody;
